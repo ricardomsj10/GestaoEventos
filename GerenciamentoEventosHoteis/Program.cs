7e916503699ef569598a619337bf54feeac37dab
Ricardo Mendes <rmendes06@hotmail.com>
2025-10-05 00:43:25 -0300

    Ajustes services

--- GerenciamentoEventosHoteis/Program.cs
+++ GerenciamentoEventosHoteis/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using GerenciamentoEventosHoteis.Data;
+using GerenciamentoEventosHoteis.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IAuditoriaService, AuditoriaService>();
 
 var app = builder.Build();
 
